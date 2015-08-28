0b36df92b0cff84b04a7c3eafc8cc264e66525cf
karthik rk <karthikrk@karthiks-MacBook-Pro.local>
2015-08-28 07:02:10 +0530

    First Blank Commit

---
+++ app/src/main/java/com/karthik/fbchatheadexample/FBChatHeadActivityFragment.java
@@ -0,0 +1,22 @@
+package com.karthik.fbchatheadexample;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class FBChatHeadActivityFragment extends Fragment {
+
+    public FBChatHeadActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_fbchat_head, container, false);
+    }
+}

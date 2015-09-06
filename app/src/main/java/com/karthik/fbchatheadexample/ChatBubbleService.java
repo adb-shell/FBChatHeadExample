52920f88c16adae8a371700046e5ef9e02f97937
callmekarthik <karthik.rk18@gmail.com>
2015-09-06 19:58:36 +0530

    RAW COMMIT-5

---
+++ app/src/main/java/com/karthik/fbchatheadexample/ChatBubbleService.java
@@ -0,0 +1,247 @@
+package com.karthik.fbchatheadexample;
+
+import android.app.IntentService;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.os.Build;
+import android.os.Handler;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by karthikrk on 04/09/15.
+ */
+public class ChatBubbleService extends Service {
+
+    private WindowManager windowManager;
+    private RelativeLayout chatheadView, removeView;
+    private Point szWindow = new Point();
+    private int x_init_cord, y_init_cord, x_init_margin, y_init_margin,x_remove,y_remove;
+    private Context mContext;
+    private ImageView chatheadImg, removeImg;
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.e("TAG","CHAT HEAD SERVICE STARTED");
+        if(startId == Service.START_STICKY) {
+            displayChatBubble();
+            return super.onStartCommand(intent, flags, startId);
+        }else{
+            return  Service.START_NOT_STICKY;
+        }
+    }
+
+    private void displayChatBubble() {
+
+        mContext = this;
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(mContext.LAYOUT_INFLATER_SERVICE);
+
+        //Adding the remove view
+        removeView = (RelativeLayout) inflater.inflate(R.layout.remove, null);
+
+        WindowManager.LayoutParams removeParams = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                PixelFormat.TRANSLUCENT);
+
+        //in order to add the remove exactly at the bottom of the screen
+        removeParams.gravity = Gravity.BOTTOM | Gravity.CENTER;
+        removeView.setVisibility(View.GONE);
+        removeImg = (ImageView) removeView.findViewById(R.id.remove_img);
+        windowManager.addView(removeView, removeParams);
+
+
+        //Adding the chathead view
+        chatheadView = (RelativeLayout) inflater.inflate(R.layout.fbchathead, null);
+        chatheadImg = (ImageView) chatheadView.findViewById(R.id.chathead_img);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            windowManager.getDefaultDisplay().getSize(szWindow);
+        } else {
+            int w = windowManager.getDefaultDisplay().getWidth();
+            int h = windowManager.getDefaultDisplay().getHeight();
+            szWindow.set(w, h);
+        }
+
+        //use the same params as that of remove view just change the gravity
+        WindowManager.LayoutParams chatHeadParams = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                PixelFormat.TRANSLUCENT);
+
+        chatHeadParams.gravity = Gravity.TOP | Gravity.LEFT;
+        chatHeadParams.x = 0;
+        chatHeadParams.y = 100;
+        windowManager.addView(chatheadView, chatHeadParams);
+
+        chatheadView.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                WindowManager.LayoutParams layoutParams = (WindowManager.LayoutParams) chatheadView.getLayoutParams();
+
+                int x_cord = (int) event.getRawX();
+                int y_cord = (int) event.getRawY();
+                int x_cord_Destination, y_cord_Destination;
+                switch (event.getAction()) {
+                    //when the user has pressed the chat bubble
+                    case MotionEvent.ACTION_DOWN:
+
+                        handler_longClick.post(runnable_longClick);
+                        remove_img_width = removeImg.getLayoutParams().width;
+                        remove_img_height = removeImg.getLayoutParams().height;
+
+                        x_init_cord = x_cord;
+                        y_init_cord = y_cord;
+
+                        x_init_margin = layoutParams.x;
+                        y_init_margin = layoutParams.y;
+                        break;
+
+                    //when the user is dragging the chat bubble
+                    case MotionEvent.ACTION_MOVE:
+                        int x_diff_move = x_cord - x_init_cord;
+                        int y_diff_move = y_cord - y_init_cord;
+
+                        x_cord_Destination = x_init_margin + x_diff_move;
+                        y_cord_Destination = y_init_margin + y_diff_move;
+
+
+                        layoutParams.x = x_cord_Destination;
+                        layoutParams.y = y_cord_Destination;
+
+                        //make the remove view bigger
+                        if(isViewIntersects(layoutParams.x, layoutParams.y)){
+                            removeImg.getLayoutParams().height = (int) (remove_img_height * 1.5);
+                            removeImg.getLayoutParams().width = (int) (remove_img_width * 1.5);
+                            windowManager.updateViewLayout(removeView, removeView.getLayoutParams());
+                            inBound = true;
+                        }
+
+                        else{
+                            //restore the height to the normal of the remove view
+                            if(inBound){
+                                removeImg.getLayoutParams().height = getPixels();
+                                removeImg.getLayoutParams().width = getPixels();
+                                windowManager.updateViewLayout(removeView, removeView.getLayoutParams());
+                                inBound = false;
+                            }
+                        }
+
+                        windowManager.updateViewLayout(chatheadView, layoutParams);
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        removeView.setVisibility(View.GONE);
+                        removeImg.getLayoutParams().height = remove_img_height;
+                        removeImg.getLayoutParams().width = remove_img_width;
+                        handler_longClick.removeCallbacks(runnable_longClick);
+
+                        int x_diff = x_cord - x_init_cord;
+                        int y_diff = y_cord - y_init_cord;
+
+                        x_cord_Destination = x_init_margin + x_diff;
+                        y_cord_Destination = y_init_margin + y_diff;
+
+
+                        int BarHeight = getStatusBarHeight();
+                        if (y_cord_Destination < 0) {
+                            y_cord_Destination = 0;
+                        } else if (y_cord_Destination + (chatheadView.getHeight() + BarHeight) > szWindow.y) {
+                            y_cord_Destination = szWindow.y - (chatheadView.getHeight() + BarHeight);
+                        }
+                        layoutParams.y = y_cord_Destination;
+
+                        if(inBound && chatheadView!=null){
+                            //remove the chathead view here
+                            windowManager.removeView(chatheadView);
+                            stopSelf();
+                            chatheadView = null;
+                        }
+                        break;
+
+                    default:
+                        Log.e("TAG", "chatheadView.setOnTouchListener  -> event.getAction() : default");
+                        break;
+                }
+                return true;
+            }
+
+            boolean inBound = false;
+            int remove_img_width = 0, remove_img_height = 0;
+
+            Handler handler_longClick = new Handler();
+            Runnable runnable_longClick = new Runnable() {
+
+                @Override
+                public void run() {
+                    removeView.setVisibility(View.VISIBLE);
+                    if(x_remove==0 || y_remove==0){
+                        int[] pos = new int[2];
+                        removeView.getLocationOnScreen(pos);
+                        x_remove = pos[0];
+                        y_remove = pos[1];
+                    }
+                }
+            };
+        });
+
+    }
+
+    private int getStatusBarHeight() {
+        int statusBarHeight = (int) Math.ceil(25 * getResources().getDisplayMetrics().density);
+        return statusBarHeight;
+    }
+
+    private boolean isViewIntersects(int x, int y) {
+        Rect removeRect = new Rect(x_remove,y_remove, x_remove+removeView.getWidth(),y_remove+removeView.getHeight());
+        Rect ChatHeadRect = new Rect(x,y, x+removeView.getWidth(),y+removeView.getHeight());
+        return removeRect.intersect(ChatHeadRect);
+    }
+
+    private int getPixels(){
+        final float scale = getResources().getDisplayMetrics().density;
+        return (int) (80 * scale + 0.5f);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        Log.e("TAG", "CHAT HEAD SERVICE DESTROYED");
+
+        if(chatheadView != null){
+            windowManager.removeView(chatheadView);
+        }
+
+        if(removeView != null){
+            windowManager.removeView(removeView);
+        }
+
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+}
--- app/src/main/java/com/karthik/fbchatheadexample/FBChatHeadActivityFragment.java
+++ app/src/main/java/com/karthik/fbchatheadexample/FBChatHeadActivityFragment.java
@@ -1,36 +1,26 @@
 package com.karthik.fbchatheadexample;
 
+import java.util.List;
+
+import android.app.ActivityManager;
+import android.app.Application;
 import android.content.Context;
-import android.graphics.PixelFormat;
-import android.graphics.Point;
-import android.graphics.Rect;
-import android.os.Build;
-import android.os.Handler;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class FBChatHeadActivityFragment extends Fragment implements View.OnClickListener{
 
-    private Context mContext;
-    private RelativeLayout chatheadView, removeView;
-    private ImageView chatheadImg, removeImg;
-    private Point szWindow = new Point();
-    private int x_init_cord, y_init_cord, x_init_margin, y_init_margin,x_remove,y_remove;
-    private WindowManager windowManager;
-
-
+    private static Context mContext;
+    private Intent serviceIntent;
     public FBChatHeadActivityFragment() {
 
     }
@@ -40,7 +30,6 @@ public class FBChatHeadActivityFragment extends Fragment implements View.OnClick
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_fbchat_head, container, false);
         mContext = getActivity();
-        windowManager = (WindowManager) mContext.getSystemService(mContext.WINDOW_SERVICE);
         setupClickListners(rootView);
         return rootView;
     }
@@ -62,186 +51,32 @@ public class FBChatHeadActivityFragment extends Fragment implements View.OnClick
                 break;
             case R.id.stop_floating:
                 //remove the chat head
-                if(chatheadView!=null){
-                    windowManager.removeView(chatheadView);
-                    chatheadView = null;
+                if(isServiceRunning("ChatBubbleService")){
+                    mContext.stopService(serviceIntent);
                 }
                 break;
         }
     }
 
 
-    private void displayButton(){
+    public static boolean isServiceRunning(String serviceClassName){
+        final ActivityManager activityManager = (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE);
+        final List<ActivityManager.RunningServiceInfo> services = activityManager.getRunningServices(Integer.MAX_VALUE);
 
-        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(mContext.LAYOUT_INFLATER_SERVICE);
-        removeView = (RelativeLayout)inflater.inflate(R.layout.remove, null);
-
-        WindowManager.LayoutParams paramRemove = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.TYPE_PHONE,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-                PixelFormat.TRANSLUCENT);
-
-        //in order to add the remove exactly at the bottom of the screen
-        paramRemove.gravity = Gravity.BOTTOM | Gravity.CENTER;
-        removeView.setVisibility(View.GONE);
-        removeImg = (ImageView)removeView.findViewById(R.id.remove_img);
-        windowManager.addView(removeView, paramRemove);
-
-
-        chatheadView = (RelativeLayout) inflater.inflate(R.layout.fbchathead, null);
-        chatheadImg = (ImageView)chatheadView.findViewById(R.id.chathead_img);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            windowManager.getDefaultDisplay().getSize(szWindow);
-        } else {
-            int w = windowManager.getDefaultDisplay().getWidth();
-            int h = windowManager.getDefaultDisplay().getHeight();
-            szWindow.set(w, h);
-        }
-
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.TYPE_PHONE,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-                PixelFormat.TRANSLUCENT);
-
-        //Please feel free to animate the incoming of the bubble and adjust its position accordingly
-        params.gravity = Gravity.TOP | Gravity.LEFT;
-        params.x = 0;
-        params.y = 100;
-        windowManager.addView(chatheadView, params);
-
-
-        chatheadView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                WindowManager.LayoutParams layoutParams = (WindowManager.LayoutParams) chatheadView.getLayoutParams();
-
-                int x_cord = (int) event.getRawX();
-                int y_cord = (int) event.getRawY();
-                int x_cord_Destination, y_cord_Destination;
-                switch (event.getAction()) {
-
-
-                    case MotionEvent.ACTION_DOWN:
-
-                        handler_longClick.post(runnable_longClick);
-
-                        remove_img_width = removeImg.getLayoutParams().width;
-                        remove_img_height = removeImg.getLayoutParams().height;
-
-                        x_init_cord = x_cord;
-                        y_init_cord = y_cord;
-
-                        x_init_margin = layoutParams.x;
-                        y_init_margin = layoutParams.y;
-                        break;
-
-                    //when the user is dragging the bubble calculate the offset
-                    case MotionEvent.ACTION_MOVE:
-
-                        int x_diff_move = x_cord - x_init_cord;
-                        int y_diff_move = y_cord - y_init_cord;
-
-                        x_cord_Destination = x_init_margin + x_diff_move;
-                        y_cord_Destination = y_init_margin + y_diff_move;
-
-
-                        layoutParams.x = x_cord_Destination;
-                        layoutParams.y = y_cord_Destination;
-
-                        //make the remove view bigger
-                        if(isViewIntersects(layoutParams.x, layoutParams.y)){
-                            removeImg.getLayoutParams().height = (int) (remove_img_height * 1.5);
-                            removeImg.getLayoutParams().width = (int) (remove_img_width * 1.5);
-                            windowManager.updateViewLayout(removeView, removeView.getLayoutParams());
-                            inBound = true;
-                        }
-
-                        else{
-                            //restore the height to the normal of the remove view
-                            if(inBound){
-                                removeImg.getLayoutParams().height = getPixels();
-                                removeImg.getLayoutParams().width = getPixels();
-                                windowManager.updateViewLayout(removeView, removeView.getLayoutParams());
-                                inBound = false;
-                            }
-                        }
-
-                        windowManager.updateViewLayout(chatheadView, layoutParams);
-                        break;
-
-                    //when user lifts his hand update the current position
-                    case MotionEvent.ACTION_UP:
-                        removeView.setVisibility(View.GONE);
-                        removeImg.getLayoutParams().height = remove_img_height;
-                        removeImg.getLayoutParams().width = remove_img_width;
-                        handler_longClick.removeCallbacks(runnable_longClick);
-
-                        int x_diff = x_cord - x_init_cord;
-                        int y_diff = y_cord - y_init_cord;
-
-                        x_cord_Destination = x_init_margin + x_diff;
-                        y_cord_Destination = y_init_margin + y_diff;
-
-
-                        int BarHeight = getStatusBarHeight();
-                        if (y_cord_Destination < 0) {
-                            y_cord_Destination = 0;
-                        } else if (y_cord_Destination + (chatheadView.getHeight() + BarHeight) > szWindow.y) {
-                            y_cord_Destination = szWindow.y - (chatheadView.getHeight() + BarHeight);
-                        }
-                        layoutParams.y = y_cord_Destination;
-
-                        if(inBound && chatheadView!=null){
-                            //remove the chathead view here
-                            windowManager.removeView(chatheadView);
-                            chatheadView = null;
-                        }
-
-                        break;
-                }
+        for (ActivityManager.RunningServiceInfo runningServiceInfo : services) {
+            Log.e("SERVICE",runningServiceInfo.process);
+            if (runningServiceInfo.service.getClassName().equals(serviceClassName)){
                 return true;
-
             }
-
-            boolean inBound = false;
-            int remove_img_width = 0, remove_img_height = 0;
-
-            Handler handler_longClick = new Handler();
-            Runnable runnable_longClick = new Runnable() {
-
-                @Override
-                public void run() {
-                    removeView.setVisibility(View.VISIBLE);
-                    if(x_remove==0 || y_remove==0){
-                        int[] pos = new int[2];
-                        removeView.getLocationOnScreen(pos);
-                        x_remove = pos[0];
-                        y_remove = pos[1];
-                    }
-                }
-            };
-
-        });
+        }
+        return false;
     }
 
-    private int getStatusBarHeight() {
-        int statusBarHeight = (int) Math.ceil(25 * getResources().getDisplayMetrics().density);
-        return statusBarHeight;
+
+    private void displayButton(){
+        //starting the chat bubble service
+        serviceIntent = new Intent(mContext, ChatBubbleService.class);
+        mContext.startService(serviceIntent);
     }
 
-    private boolean isViewIntersects(int x, int y) {
-        Rect removeRect = new Rect(x_remove,y_remove, x_remove+removeView.getWidth(),y_remove+removeView.getHeight());
-        Rect ChatHeadRect = new Rect(x,y, x+removeView.getWidth(),y+removeView.getHeight());
-        return removeRect.intersect(ChatHeadRect);
-    }
-
-    private int getPixels(){
-        final float scale = getResources().getDisplayMetrics().density;
-        return (int) (80 * scale + 0.5f);
-    }
 }
